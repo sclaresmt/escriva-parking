9a8fa33ad30b7ac849ac62e64b364e8b58aedcec
sclaresmt <sclaresmt@gmail.com>
2024-06-07 18:04:40 +0200

    Añadido nuevo activity que debe abrirse al leer NFC

--- app/src/main/java/es/escriva/MainActivity.kt
+++ app/src/main/java/es/escriva/MainActivity.kt
@@ -1,5 +1,9 @@
 package es.escriva
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,14 +11,51 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val nfcIntentFilter = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED).apply {
+        try {
+            addDataType("*/*") // Para manejar todos los tipos MIME
+        } catch (e: IntentFilter.MalformedMimeTypeException) {
+            throw RuntimeException("Error al añadir tipo MIME", e)
+        }
+    }
+
+    private val nfcPendingIntent = PendingIntent.getActivity(
+        this, 0, Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+        PendingIntent.FLAG_MUTABLE // Requerido en Android 12 y superior
+    )
+
+    private lateinit var nfcAdapter: NfcAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constraint_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
     }
+
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter?.enableForegroundDispatch(this, nfcPendingIntent, arrayOf(nfcIntentFilter), null)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
+            // Aquí puedes procesar la información del tag NFC si es necesario
+            startActivity(Intent(this, NfcAction::class.java))
+        }
+    }
+
 }
